f9abda94dc139253fe5be3b790381e4e239357b9
ErikkunChan <erikku.chan.97@gmail.com>
2019-05-17 19:33:54 -0400

    Update PiCamera.py

--- PiCamera.py
+++ PiCamera.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Libraries
+# Main Modules
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from picamera import PiCamera
@@ -9,31 +9,48 @@ import smtplib
 from time import sleep
 import RPi.GPIO as gpio
 import datetime
+# AWS Modules
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import logging
+import time
+import argparse
+import json
+# AWS Configuration
+host = "a1cai3u3kh553t-ats.iot.us-east-2.amazonaws.com"
+certPath = "/home/pi/4925Project/"
+clientId = "photon"
+topic = "doorbell"
 
-# Experimental ------------------------------------
-#import boto3
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = None
+myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+myAWSIoTMQTTClient.configureCredentials("{}AmazonRootCA1.cer".format(certPath), "{}iot-private.pem.key".format(certPath), "{}iot-certificate.pem.crt".format(certPath))
 
-#AWS_ACCESS_KEY = 'INSERT_AWS_ACCESS_KEY'
-#AWS_SECRET_ACCESS = 'INSERT_AWS_SECRET_ACCESS'
-#TOPIC_ARN = 'INSERT_TOPIC_ARN' # should similar with this arn:aws:sns:us-east-1:1234567890:door-status
-# --------------------------------------------------
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+myAWSIoTMQTTClient.connect()
 
+# Set the pin mode 
 gpio.setmode(gpio.BCM)
 
 # Connecting push button to GPIO Pin 18
 gpio.setup(18, gpio.IN, pull_up_down=gpio.PUD_UP)
 
 # Initializes the Pi Camera and sets it for night time photos
-camera = PiCamera(resolution = (1280, 720),
-		  framerate = Fraction(1, 6),
-		  sensor_mode = 3)
-camera.shutter_speed = 6000000
-camera.iso = 800
+camera = PiCamera(resolution = (1280, 720))
 
 #Setting up email parameters
 toAddr = 'iotdoorbell@outlook.com'
 fromAddr = 'iotdoorbell@outlook.com'
 subject = 'Doorbell pressed, someone is at the door'
+
+# Publish to the same topic in a loop forever
+loopCount = 0
 
 while True:
 	
@@ -75,15 +92,11 @@ while True:
 
 		print("Email Sent")
 		
-		# Experimental ---------------------------------------
-		#snsClient = boto3.client(
-            	#'sns',
-            	#aws_access_key_id = AWS_ACCESS_KEY,
-            	#aws_secret_access_key = AWS_SECRET_ACCESS,
-            	#region_name = 'us-east-2'
-        	#)
-		
-		#message = "Someone is at the door" 
-                #print (message)
-                #publish(snsClient, message)
-		# -----------------------------------------------------
+    		message = {}
+   		message['message'] = "Someone is at the door!"
+    		messageJson = json.dumps(message)
+	    	myAWSIoTMQTTClient.publish(topic, messageJson, 1)
+    		print('Published topic %s: %s\n' % (topic, messageJson))
+    		loopCount += 1
+    		time.sleep(10)
+myAWSIoTMQTTClient.disconnect()
